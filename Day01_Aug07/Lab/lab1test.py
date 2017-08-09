e27a05e6bb3a17863b4d8220517d9e900533fa3f
ylw12 <luwei.ying@wustl.edu>
2017-08-09 10:05:02 -0500

    test

--- Day01_Aug07/Lab/lab1test.py
+++ Day01_Aug07/Lab/lab1test.py
@@ -1,7 +1,7 @@
 import unittest
-import lab1_dc
+import Aug7_exercise
 
-class Testlab1_dcCode(unittest.TestCase):
+class TestAug7_exerciseCode(unittest.TestCase):
 
 	def setUp(self): 
 		self.x2 = "10000"
@@ -14,84 +14,84 @@ class Testlab1_dcCode(unittest.TestCase):
 
 	# binarify
 	def test_binarify_16(self):
-		self.assertEqual(lab1_dc.binarify(16), "10000")
+		self.assertEqual(Aug7_exercise.binarify(16), "10000")
 
 	def test_binarify_127(self):
-		self.assertEqual(lab1_dc.binarify(127), "1111111")
+		self.assertEqual(Aug7_exercise.binarify(127), "1111111")
 
 	def test_binarify_0(self):
-		self.assertEqual(lab1_dc.binarify(0), "0")
+		self.assertEqual(Aug7_exercise.binarify(0), "0")
 
 	def test_binarify_negative(self):
-		self.assertEqual(lab1_dc.binarify(-16), "0")
+		self.assertEqual(Aug7_exercise.binarify(-16), "0")
 
 
 	# int_to_base
 	def test_int_to_base_2(self):
-		self.assertEqual(lab1_dc.int_to_base(123, 2), lab1_dc.binarify(123))
+		self.assertEqual(Aug7_exercise.int_to_base(123, 2), Aug7_exercise.binarify(123))
 
 	def test_int_to_base_3(self):
-		self.assertEqual(lab1_dc.int_to_base(12, 3), "110")
+		self.assertEqual(Aug7_exercise.int_to_base(12, 3), "110")
 
 	def test_int_to_base_10(self):
-		self.assertEqual(lab1_dc.int_to_base(16, 10), "16")
+		self.assertEqual(Aug7_exercise.int_to_base(16, 10), "16")
 
 	def test_int_to_base_0(self):
-		self.assertEqual(lab1_dc.int_to_base(16, 0), "0")
+		self.assertEqual(Aug7_exercise.int_to_base(16, 0), "0")
 
 	def test_int_to_base_negative(self):
-		self.assertEqual(lab1_dc.int_to_base(-16, 2), "-10000")
+		self.assertEqual(Aug7_exercise.int_to_base(-16, 2), "-10000")
 
 
 	# base_to_int
 	def test_base_to_int_16(self):
-		self.assertEqual(lab1_dc.base_to_int("10000", 2), 16)
+		self.assertEqual(Aug7_exercise.base_to_int("10000", 2), 16)
 
 	def test_base_to_int_3(self):
-		self.assertEqual(lab1_dc.base_to_int("110", 3), 12)
+		self.assertEqual(Aug7_exercise.base_to_int("110", 3), 12)
 
 	def test_base_to_int_10(self):
-		self.assertEqual(lab1_dc.base_to_int("16", 10), 16)
+		self.assertEqual(Aug7_exercise.base_to_int("16", 10), 16)
 
 	def test_base_to_int_0(self):
-		self.assertEqual(lab1_dc.base_to_int("123", 0), 0)
+		self.assertEqual(Aug7_exercise.base_to_int("123", 0), 0)
 
 	def test_base_to_int_negative(self):
-		self.assertEqual(lab1_dc.base_to_int("-10000", 2), -16)
+		self.assertEqual(Aug7_exercise.base_to_int("-10000", 2), -16)
 
 
 	# flexibase methods
 	def test_flexibase_add_base_2(self):
-		self.assertEqual(lab1_dc.flexibase_add(self.x2, self.y2, 2, 2), self.xplusy)
+		self.assertEqual(Aug7_exercise.flexibase_add(self.x2, self.y2, 2, 2), self.xplusy)
 
 	def test_flexibase_add_base_2_10(self):
-		self.assertEqual(lab1_dc.flexibase_add(self.x2, self.y10, 2, 10), self.xplusy)
+		self.assertEqual(Aug7_exercise.flexibase_add(self.x2, self.y10, 2, 10), self.xplusy)
 
 	def test_flexibase_multiply_base_2(self):
-		self.assertEqual(lab1_dc.flexibase_multiply(self.x2, self.y2, 2, 2), self.xtimesy)
+		self.assertEqual(Aug7_exercise.flexibase_multiply(self.x2, self.y2, 2, 2), self.xtimesy)
 
 	def test_flexibase_multiply_base_10(self):
-		self.assertEqual(lab1_dc.flexibase_multiply(self.x2, self.y10, 2, 10), self.xtimesy)
+		self.assertEqual(Aug7_exercise.flexibase_multiply(self.x2, self.y10, 2, 10), self.xtimesy)
 
 
 	# romanify 
 	def test_romanify_3(self):
-		self.assertEqual(lab1_dc.romanify(3), "III")
+		self.assertEqual(Aug7_exercise.romanify(3), "III")
 
 	def test_romanify_9(self):
-		self.assertEqual(lab1_dc.romanify(9), "IX")
+		self.assertEqual(Aug7_exercise.romanify(9), "IX")
 
 	def test_romanify_10(self):
-		self.assertEqual(lab1_dc.romanify(10), "X")
+		self.assertEqual(Aug7_exercise.romanify(10), "X")
 
 	def test_romanify_49(self):
-		self.assertEqual(lab1_dc.romanify(49), "XLIX")
+		self.assertEqual(Aug7_exercise.romanify(49), "XLIX")
 
 	def test_romanify_99(self):
-		self.assertEqual(lab1_dc.romanify(99), "XCIX")
+		self.assertEqual(Aug7_exercise.romanify(99), "XCIX")
 
 	def test_romanify_3999(self):
-		self.assertEqual(lab1_dc.romanify(3999), "MMMCMXCIX")
+		self.assertEqual(Aug7_exercise.romanify(3999), "MMMCMXCIX")
 
 
 if __name__ == '__main__':
