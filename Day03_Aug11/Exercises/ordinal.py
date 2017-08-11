b1c5ee6a2fdc579a14e03847576cd387e9b9e9bd
ylw12 <luwei.ying@wustl.edu>
2017-08-11 11:38:18 -0500

    ordinal

--- Day03_Aug11/Exercises/ordinal.py
+++ Day03_Aug11/Exercises/ordinal.py
@@ -2,7 +2,31 @@
 #For example, for 1, it should give "1st"
 #Write a test for your function
 
-def myfunction(i):
-	return i
+def myfunction(entry):
+	if type(entry) == int:
+	    if len(str(entry)) == 1:
+	    	if entry == '1': 
+	    		return '1st'
+	        elif entry == '2': 
+	        	return '2nd'
+	        elif entry == '3': 
+	        	return '3rd'
+	        else: 
+	        	return '%sth' %(entry)
+	    else:
+	    	entry = str(entry)
+   		    if entry[-2] == '1':
+  			    return "%sth" %(entry)
+   		    elif entry[-1] == '1':
+   			    return "%sst" %(entry)
+   		    elif entry[-1] == '2':
+   			    return "%snd" %(entry)
+   		    elif entry[-1] == '3':
+   			    return "%srd" %(entry)
+   		    else:
+   			    return  "%sth" %(entry)
+    else:
+        return "Please enter an integer."
+
 
 print myfunction(1)	
