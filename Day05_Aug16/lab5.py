31ae62d0ccdc498ec2f99a743ce0d78d2042511b
ylw12 <luwei.ying@wustl.edu>
2017-08-16 14:16:19 -0500

    Lab5

--- Day05_Aug16/lab5.py
+++ Day05_Aug16/lab5.py
@@ -3,7 +3,7 @@ from googlemaps import Client
 from datetime import datetime
 
 
-api_key = 'AIzaSyDWQH2vuIJSMTWA5T0Cd0Y2cfcfX6TIl70'
+api_key = 'your key'
 gmaps = Client(api_key)
 dir(gmaps)
 whitehouse = '1600 Pennsylvania Avenue, Washington, DC'
@@ -40,7 +40,61 @@ embassies = [[38.917228,-77.0522365],
 
 # TODO: write code to answer the following questions: 
 # which embassy is closest to the White House in meters? how far? 
-# what is its address? 
-# if I wanted to hold a morning meeting there, which cafe would you suggest?
-# if I wanted to hold an evening meeting there, which bar would you suggest? 
+embassy_A = gmaps.reverse_geocode(embassies[0])[0]['formatted_address']
+embassy_B = gmaps.reverse_geocode(embassies[1])[0]['formatted_address']
+embassy_C = gmaps.reverse_geocode(embassies[2])[0]['formatted_address']
+now = datetime.now()
+#A
+directionA = gmaps.directions(embassy_A,
+                              whitehouse,
+                              mode="transit",
+                              departure_time=now)
+print directionA[0]['legs'][0]['distance']
+#B
+directionB = gmaps.directions(embassy_B,
+                              whitehouse,
+                              mode="transit",
+                              departure_time=now)
+print directionB[0]['legs'][0]['distance']
+#C
+directionC = gmaps.directions(embassy_C,
+                              whitehouse,
+                              mode="transit",
+                              departure_time=now)
+print directionC[0]['legs'][0]['distance']
+# Answer: Embassy B is the closest. It is 0.8/1.6*1000 meters.
 
+# what is its address? 
+embassy_B # '1601 Massachusetts Ave NW, Washington, DC 20036, USA'
+
+# if I wanted to hold a morning meeting there, which cafe would you suggest?
+cafes = gmaps.places('cafe near ' + embassy_B)
+print cafes['results'][0]['formatted_address'] # check one of them
+print cafes['results'][0]['name']
+# Compare the distances
+for i in range(0, len(cafes)):
+	cafe_address = cafes['results'][i]['formatted_address']
+	print cafe_address
+	test = gmaps.directions(embassy_B, cafe_address, mode="transit", departure_time=now) #Look up epoch time
+	print test[0]['legs'][0]['distance'] #%cafes['results'][i]['name']
+	print cafes['results'][i]['name']
+# I would recommance this because it is the nearest:
+#1625 Massachusetts Ave NW, Washington, DC 20008, United States
+#{u'text': u'302 ft', u'value': 92}
+#Galley Cafe
+
+# if I wanted to hold an evening meeting there, which bar would you suggest? 
+bars = gmaps.places('bar near ' + embassy_B)
+print bars['results'][0:len(bars)]['formatted_address'] # check one of them
+print bars['results'][0:len(bars)]['name']
+# Compare the ratings
+for i in range(0, len(bars)):
+	bar_address = bars['results'][i]['formatted_address']
+	print bar_address
+	#test = gmaps.directions(embassy_B, bar_address, mode="transit", departure_time=now)
+	print bars['results'][i]['rating'] #%cafes['results'][i]['name']
+	print bars['results'][i]['name']
+# I would recommand this because it has the highest rating.
+#300 Massachusetts Ave NW, Washington, DC 20001, United States
+#4.5
+#Carving Room Kitchen & Bar
