22b8e84fc39a4e830756c8bc29bf035b5b8ccc04
ylw12 <luwei.ying@wustl.edu>
2017-08-22 14:08:10 -0500

    Lab8

--- Day08_Aug22/Lab/lab8.py
+++ Day08_Aug22/Lab/lab8.py
@@ -14,9 +14,21 @@ for line in text:
     print line 
 
 # TODO: print all lines that DO NOT contain "the "
+for line in text:
+  if not keyword.findall(line):
+    print line 
+
 # TODO: print lines that contain a word of any length starting with s and ending with e
-  
-# date = raw_input("Please enter a date in the format MM.DD.YY: ")
+pattern = re.compile(r'\b[Ss]\w*e\b') 
+for line in text:
+	if pattern.findall(line):
+		print line
+
+date = raw_input("Please enter a date in the format MM.DD.YY: ")
+pattern = re.compile(r'(\d\d)\.(\d\d)\.(\d\d)')
+print 'Month: %s \nDay: %s \nYear: %s' %(pattern.search(date).group(1), pattern.search(date).group(2), pattern.search(date).group(3))
+
+
 # Print the date input in the following format:
 # Month: MM
 # Day: DD
