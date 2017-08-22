128c8aa8191c0c10f75ede42f2a24752c81a7c19
ylw12 <luwei.ying@wustl.edu>
2017-08-22 16:11:38 -0500

    Finish HW4

--- Homeworks/HW4/hw4.py
+++ Homeworks/HW4/hw4.py
@@ -1,3 +1,12 @@
+import timeit
+import random
+import numpy
+import matplotlib.pyplot as plt
+
+# Create some lists for tests.
+list1 = [9, 1, 6, 2, 7, 12, 0]
+list2 = [99, 141, 6, 2, 17, 1212, 0]
+
 # Creating the function for INSERTION SORT.
 def InsertionSort(list):
 	if len(list) == 0:
@@ -14,6 +23,9 @@ def InsertionSort(list):
 			index += 1
 		return list
 
+InsertionSort(list1)
+InsertionSort(list2)
+
 # Creating the function for SELECTION SORT.
 def SelectionSort(list):
 	if len(list) == 0:
@@ -27,3 +39,36 @@ def SelectionSort(list):
 			list.insert(index,target)
 			index += 1
 		return list
+
+SelectionSort(list1)
+SelectionSort(list2)
+
+# Define a fuction for simulation.
+def SimSort(N):
+	list = random.sample(range(10000), N)
+	# Record the time for InsertionSort()
+	start_time = timeit.default_timer()
+	InsertionSort(list)
+	Insertion_t = timeit.default_timer() - start_time
+	# Record the time for SelectionSort()
+	start_time = timeit.default_timer()
+	SelectionSort(list)
+	Selection_t = timeit.default_timer() - start_time
+	return Insertion_t, Selection_t
+
+# Run the simulation for several times and record the results.
+InsertionTimeTrend = []
+SelectionTimeTrend = []
+for i in range(100, 10000, 200):
+	result = SimSort(i)
+	InsertionTimeTrend.append(result[0])
+	SelectionTimeTrend.append(result[1])
+
+# Plot the two trends above.
+plt.plot(range(1, 10000, 200), InsertionTimeTrend, 
+	range(1, 10000, 200), SelectionTimeTrend)
+plt.legend(['Insertion Sort', 'Selection Sort'], loc = "upper left", prop = {"size":10})
+plt.ylabel("Time in Seconds")
+plt.xlabel("Sample Size")
+plt.title("The Effect of Different Sort Algorithms on Runtime")
+plt.savefig('hw4_plot.pdf')
